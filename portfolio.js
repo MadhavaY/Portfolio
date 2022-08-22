34d28535c323d1a43ad5755e776575ed0f41a7d7
Madhava <madhavayedla@gmail.com>
2022-08-22 18:42:09 +0530

    Added blogs and emojis

--- portfolio.js
+++ portfolio.js
@@ -77,7 +77,7 @@ function myFunction() {
       "text"    : `Random Quotes generator made using Javascript. All the new quotes are generated using an API. Find the source code of the webpage here :
 
 
-      <a href="https://github.com/MadhavaY/grubhub" target="_blank">
+      <a href="https://github.com/MadhavaY/RandomQuotesGenerator" target="_blank">
 
 
 
@@ -89,8 +89,31 @@ function myFunction() {
                   d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
               </path>
           </svg>`
-  }
+  },
+
+
+  {
+    "heading"  : "Netflix",
+    "img"  :   "./project2.png",
+    "text"    : `Netflix sign in page with form validation. Find the source code of the webpage here :
+
+
+    <a href="https://github.com/MadhavaY/Netflix-SignIn-page" target="_blank">
+
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" id="git-project-link" role="img"
+            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+            stroke-linecap="round" stroke-linejoin="round" class="feather feather-github">
+            <title>GitHub</title>
+            <path
+                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
+            </path>
+        </svg>`
+}
   ]
+
+  
 
   let nxtarrow = document.getElementById('next-arrow');
   let prevarrow = document.getElementById('prev-arrow');
@@ -106,6 +129,7 @@ function myFunction() {
 
   nxtarrow.onclick = function(){
 
+    console.log('clicked')
     if(currentHead == (Projects.length -1)){
        currentHead = 0;
     }
@@ -121,6 +145,27 @@ function myFunction() {
     img.src = imgsrc;
 
     }
+
+
+
+    prevarrow.onclick = function(){
+
+      console.log('clicked')
+      if(currentHead == (Projects.length +1)){
+         currentHead = 0;
+      }
+      else{
+        currentHead++;
+      }
+  
+      var title = Projects[currentHead].heading;
+      var para = Projects[currentHead].text;
+      var imgsrc = Projects[currentHead].img;
+      heading.innerHTML = title;
+      text.innerHTML = para;
+      img.src = imgsrc;
+  
+      }
 
   
 // ********** Blogs *********
