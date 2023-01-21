f1dc0f123dee646a728a51e53b20fa6d125cd095
Madhava <madhavayedla@gmail.com>
2023-01-21 11:08:27 +0530

    Added hosted links to project

--- portfolio.js
+++ portfolio.js
@@ -83,8 +83,7 @@ leftHiddenElements.forEach((leftel)=>leftObserver.observe(leftel));
     {
       "heading" : "Online Library website",
       "img" :      "./project3.png",
-      "text"  : `This is a website to manage a library online. Made using Bootstrap and JavaScript. You can add a book to the library and remove it effortlessly. 
-      Find the source code for the project here :
+      "text"  : `
 
       <a href="https://github.com/MadhavaY/Library-website-using-Javascript" target="_blank">
 
@@ -104,7 +103,7 @@ leftHiddenElements.forEach((leftel)=>leftObserver.observe(leftel));
      {
         "heading"  : "Food ordering website",
         "img"  :  "./project4.png",  
-        "text"     : `Food ordering web page like zomato or swiggy. Fully responsive website made using HTML and CSS. Find the source code of the webpage here :
+        "text"     : `
 
 
         <a href="https://github.com/MadhavaY/grubhub" target="_blank">
@@ -118,13 +117,19 @@ leftHiddenElements.forEach((leftel)=>leftObserver.observe(leftel));
                 <path
                     d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
                 </path>
-            </svg> `
+            </svg> 
+            
+            <a href="https://grubhubproject.vercel.app/" target="_blank">
+
+                            <img src="./link.png" class="link" id="git-project-link" alt="">
+                        </a>
+            `
     },
 
     {
       "heading"  : "Random Quotes",
       "img"  :   "./project5.png",
-      "text"    : `Random Quotes generator made using Javascript. All the new quotes are generated using an API. Find the source code of the webpage here :
+      "text"    : `
 
 
       <a href="https://github.com/MadhavaY/RandomQuotesGenerator" target="_blank">
@@ -138,14 +143,20 @@ leftHiddenElements.forEach((leftel)=>leftObserver.observe(leftel));
               <path
                   d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
               </path>
-          </svg>`
+          </svg>
+          
+          <a href="https://infinitequote.netlify.app/" target="_blank">
+
+                            <img src="./link.png" class="link" id="git-project-link" alt="">
+                        </a>
+          `
   },
 
 
   {
     "heading"  : "Netflix",
     "img"  :   "./project2.png",
-    "text"    : `Netflix sign in page with form validation. Find the source code of the webpage here :
+    "text"    : `
 
 
     <a href="https://github.com/MadhavaY/Netflix-SignIn-page" target="_blank">
@@ -159,7 +170,13 @@ leftHiddenElements.forEach((leftel)=>leftObserver.observe(leftel));
             <path
                 d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22">
             </path>
-        </svg>`
+        </svg>
+        
+        <a href="https://grubhubproject.vercel.app/" target="_blank">
+
+                            <img src="./link.png" class="link" id="git-project-link" alt="">
+                        </a>
+        `
 }
   ]
 
